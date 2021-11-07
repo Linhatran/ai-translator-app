768b5c501522fab4d48778df44262c8222f911b1
Linh Tran <linhtanl51@gmail.com>
2021-11-06 19:49:35 -0700

    import libraries

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, redirect, url_for, request, render_template, session
+import requests, os, uuid, json
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
